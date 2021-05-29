20aee891bf6b807ad73e4725ce2e796f71ac54c3
anonymail-app <85013225+anonymail-app@users.noreply.github.com>
2021-05-29 14:35:58 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const ejs = require('ejs');
+require('dotenv/config');
+
+const app = express();
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/send', (req, res) => {
+
+    const transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: process.env.ACCOUNT,
+    pass: process.env.PASSWORD
+  }
+}));
+
+const mailOptions = {
+  from: '"Anonymail" node.anonymail@gmail.com',
+  to: req.body.to,
+  cc: req.body.cc,
+  subject: req.body.subject + ' - Anonymail',
+  text: 'Your mail client is not compatible with web markup!',
+  html: req.body.message +  '\nThis message was was sent using Anonymail.'
+};
+
+res.render('sent')
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});  
+  
+  });
+
+app.listen(3000, () => console.log('Server started...'));
